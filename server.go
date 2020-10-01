fa3433336a62ea0f8cbcfbd268d2faeef775d3ba
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2020-10-01 22:55:47 +0200

    Add basic server

---
+++ server.go
@@ -0,0 +1,42 @@
+package plasma
+
+import (
+	"net"
+)
+
+const defaultAddr string = ":25565"
+
+// Server defines the struct of a running Minecraft server
+type Server struct {
+	Addr     string
+	listener net.Listener
+}
+
+func (srv *Server) ListenAndServe() error {
+	addr := srv.Addr
+	if addr == "" {
+		addr = defaultAddr
+	}
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	srv.listener = l
+
+	for {
+		_, err := l.Accept()
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (srv Server) Close() error {
+	if srv.listener != nil {
+		return srv.listener.Close()
+	}
+
+	return nil
+}
