8e001d64e2071a0bfb2e44d6d687104ed45e407a
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2020-10-06 09:51:19 +0200

    Add dimension consts

---
+++ protocol/v1.16.3/play/dimension.go
@@ -0,0 +1,9 @@
+package play
+
+import "github.com/spookspace/plasma/protocol/packet"
+
+const (
+	DimensionNether    = packet.Int(-1)
+	DimensionOverworld = packet.Int(0)
+	DimensionEnd       = packet.Int(1)
+)
