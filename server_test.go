117c7ed5961f075ad31afd4128ee81a76fc5ff84
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2021-01-26 05:36:19 +0100

    test: add TestStatusResponse_JSON

--- server_test.go
+++ server_test.go
@@ -1,71 +1,90 @@
 package plasma
 
 import (
+	"encoding/json"
 	"testing"
 )
 
-func TestServer_ListenAndServe(t *testing.T) {
-	/*tt := []struct {
-		srv  Server
-		addr string
-		err  string
+func TestStatusResponse_JSON(t *testing.T) {
+	tt := []struct {
+		statusResponse StatusResponse
+		expectedJSON   string
 	}{
 		{
-			srv:  Server{},
-			addr: "[::]:25565",
+			statusResponse: StatusResponse{
+				Version: Version{
+					Name:           "1.16.4",
+					ProtocolNumber: 754,
+				},
+				Motd:       "test",
+				MaxPlayers: 20,
+			},
+			expectedJSON: "{\"version\":{\"name\":\"1.16.4\",\"protocol\":754},\"players\":{\"max\":20,\"online\":0," +
+				"\"sample\":null},\"description\":{\"text\":\"test\"},\"favicon\":\"\"}",
 		},
 		{
-			srv:  Server{Addr: ":25565"},
-			addr: "[::]:25565",
+			statusResponse: StatusResponse{
+				Version: Version{
+					Name:           "1.16.4",
+					ProtocolNumber: 754,
+				},
+				Motd:          "test",
+				MaxPlayers:    20,
+				PlayersOnline: 20,
+				Players: []struct {
+					Name string
+					ID   string
+				}{
+					{
+						Name: "Haveachin",
+						ID:   "1234",
+					},
+				},
+			},
+			expectedJSON: "{\"version\":{\"name\":\"1.16.4\",\"protocol\":754},\"players\":{\"max\":20,\"online\":20," +
+				"\"sample\":[{\"name\":\"Haveachin\",\"id\":\"1234\"}]},\"description\":{\"text\":\"test\"}," +
+				"\"favicon\":\"\"}",
 		},
 		{
-			srv:  Server{Addr: ":1337"},
-			addr: "[::]:1337",
-		},
-		{
-			srv:  Server{Addr: "1337"},
-			addr: "[::]:1337",
-		},
-		{
-			srv:  Server{Addr: "abc"},
-			addr: "abc",
-			err:  "listen tcp: address abc: missing port in address",
+			statusResponse: StatusResponse{
+				Version: Version{
+					Name:           "1.16.4",
+					ProtocolNumber: 754,
+				},
+				Motd:          "test",
+				MaxPlayers:    20,
+				PlayersOnline: 20,
+				Players: []struct {
+					Name string
+					ID   string
+				}{
+					{
+						Name: "mb175",
+						ID:   "175",
+					},
+				},
+				IconPath: "./.test/test-icon-64x64.png",
+			},
+			expectedJSON: "{\"version\":{\"name\":\"1.16.4\",\"protocol\":754},\"players\":{\"max\":20,\"online\":20," +
+				"\"sample\":[{\"name\":\"mb175\",\"id\":\"175\"}]},\"description\":{\"text\":\"test\"},\"favicon\":" +
+				"\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAAAXNSR0IArs4c6QAAAARnQU1BAACx" +
+				"jwv8YQUAAAAJcEhZcwAAFiUAABYlAUlSJPAAAABeSURBVGhD7c9BCQAwDACxCemz/p3Nw2Qcg0AM5NzZrwnUBGoCNYGaQE2gJlAT" +
+				"qAnUBGoCNYGaQE2gJlATqAnUBGoCNYGaQE2gJlATqAnUBGoCNYGaQE2gJlD7PDD7AFpP0Q+6dA8hAAAAAElFTkSuQmCC\"}",
 		},
 	}
 
 	for _, tc := range tt {
-		go func() {
-			if err := tc.srv.ListenAndServe(); err != nil {
-				if err.Error() != tc.err {
-					t.Error(err)
-				}
-			}
-		}()
-
-		waitForListener := func() <-chan bool {
-			b := make(chan bool, 1)
-			for tc.srv.listener == nil {
-			}
-			b <- true
-			return b
+		srJson, err := tc.statusResponse.JSON()
+		if err != nil {
+			t.Error(err)
+		}
+		bb, err := json.Marshal(srJson)
+		if err != nil {
+			t.Error(err)
 		}
 
-		select {
-		case <-time.After(time.Second):
-			t.Fail()
-		case <-waitForListener():
-		}
-
-		if tc.srv.listener.Addr().Network() != "tcp" {
-			_ = tc.srv.Close()
+		if tc.expectedJSON != string(bb) {
 			t.Fail()
 		}
-
-		if tc.srv.listener.Addr().String() != tc.addr {
-			_ = tc.srv.Close()
-			t.Fail()
-		}
-
-		_ = tc.srv.Close()
-	}*/
+	}
 }
